593cc507a073d4052d8a23210ac7e12bfe48a806
Shivansh Srivastava <159248156+Paraphylus@users.noreply.github.com>
2025-01-18 23:34:43 +0530

    Added files
    Added Visionize main.py and index.html(inside template folder) files along with requirements.txt

---
+++ Visionizer/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, send_file, jsonify
+from huggingface_hub import InferenceClient
+from PIL import Image
+import io
+import os
+
+app = Flask(__name__)
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generateimages/<prompt>')
+def generate(prompt):
+    try:
+        print(f"Received prompt: {prompt}")
+
+        # Initialize the client with the model and token
+        client = InferenceClient("digiplay/AbsoluteReality_v1.8.1", token="HF_TOKEN")
+
+        print("Client initialized successfully.")
+
+        # Generate the image
+        image = client.text_to_image(prompt)
+        print("Image generation completed.")
+        
+        # Save the image to an in-memory buffer
+        img_io = io.BytesIO()
+        image.save(img_io, format="PNG")
+        img_io.seek(0)
+
+        return send_file(img_io, mimetype="image/png")
+    except Exception as e:
+        print(f"Error generating image for prompt '{prompt}': {e}")
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
